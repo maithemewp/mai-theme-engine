ea031a3b54f96bf7843e7423485c083cdc6d4bd3
Mike Hemberger <mike@thestizmedia.com>
2017-04-25 14:17:36 -0400

    DIR to URL constant - whoops

--- lib/scripts.php
+++ lib/scripts.php
@@ -8,8 +8,8 @@ function mai_lib_enqueue_scripts() {
 	$suffix = mai_get_suffix();
 
 	// Register script for later use
-	wp_register_script( 'mai-slick', MAITHEME_ENGINE_PLUGIN_PLUGIN_DIR . "assets/js/slick{$suffix}.js", array( 'jquery' ), '1.6.0', true );
-	wp_register_script( 'mai-slick-init', MAITHEME_ENGINE_PLUGIN_PLUGIN_DIR . "assets/js/slick-init{$suffix}.js", array( 'mai-slick' ), MAITHEME_ENGINE_PLUGIN_VERSION, true );
+	wp_register_script( 'mai-slick', MAITHEME_ENGINE_PLUGIN_PLUGIN_URL . "assets/js/slick{$suffix}.js", array( 'jquery' ), '1.6.0', true );
+	wp_register_script( 'mai-slick-init', MAITHEME_ENGINE_PLUGIN_PLUGIN_URL . "assets/js/slick-init{$suffix}.js", array( 'mai-slick' ), MAITHEME_ENGINE_PLUGIN_VERSION, true );
 }
 
 // Enqueue CSS files
@@ -19,7 +19,7 @@ function mai_lib_enqueue_styles() {
 	// Use minified files if script debug is not being used
 	$suffix = mai_get_suffix();
 
-	wp_enqueue_style( 'flexington', MAITHEME_ENGINE_PLUGIN_PLUGIN_DIR . "assets/css/flexington{$suffix}.css", array(), '2.3.5' );
+	wp_enqueue_style( 'flexington', MAITHEME_ENGINE_PLUGIN_PLUGIN_URL . "assets/css/flexington{$suffix}.css", array(), '2.3.5' );
 }
 
 // Enqueue Slick and Slick init JS files only when a slider is being used
