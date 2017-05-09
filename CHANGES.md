621ad9afe48f0e651191a57bd590955f697de55a
Mike Hemberger <mike@thestizmedia.com>
2017-05-09 09:29:33 -0400

    more padding on section wraps. utility nav wrap

--- CHANGES.md
+++ CHANGES.md
@@ -1,4 +1,4 @@
-#### 0.0.7
+#### 1.0.0-beta.1
 * Initial beta release
 * Make header-shrink more pronounced
 
--- maitheme-engine.php
+++ maitheme-engine.php
@@ -5,7 +5,7 @@
  * Plugin URI:      https://github.com/bizbudding/maitheme-engine/
  * Description:     The Mai Theme engine
  *
- * Version:         0.0.7
+ * Version:         1.0.0-beta.1
  *
  * GitHub URI:      bizbudding/maitheme-engine
  *
@@ -92,7 +92,7 @@ final class Mai_Theme_Engine {
 
         // Plugin version.
         if ( ! defined( 'MAITHEME_ENGINE_PLUGIN_VERSION' ) ) {
-            define( 'MAITHEME_ENGINE_PLUGIN_VERSION', '0.0.7' );
+            define( 'MAITHEME_ENGINE_PLUGIN_VERSION', '1.0.0-beta.1' );
         }
 
         // Plugin Folder Path.
