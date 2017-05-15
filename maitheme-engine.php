c62a99f4e477448fef4ea5fa9817e67be9a13d28
Mike Hemberger <mike@thestizmedia.com>
2017-05-15 17:10:14 -0400

    v bump

--- maitheme-engine.php
+++ maitheme-engine.php
@@ -5,7 +5,7 @@
  * Plugin URI:      https://github.com/bizbudding/maitheme-engine/
  * Description:     The Mai Theme engine
  *
- * Version:         1.0.0-beta.4
+ * Version:         1.0.0-beta.5
  *
  * GitHub URI:      bizbudding/maitheme-engine
  *
@@ -92,7 +92,7 @@ final class Mai_Theme_Engine {
 
         // Plugin version.
         if ( ! defined( 'MAITHEME_ENGINE_PLUGIN_VERSION' ) ) {
-            define( 'MAITHEME_ENGINE_PLUGIN_VERSION', '1.0.0-beta.4' );
+            define( 'MAITHEME_ENGINE_PLUGIN_VERSION', '1.0.0-beta.5' );
         }
 
         // Plugin Folder Path.
