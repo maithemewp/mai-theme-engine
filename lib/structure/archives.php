4bfc85df117a7d451eed324a028ef82148b021e6
Mike Hemberger <mike@thestizmedia.com>
2017-05-22 10:59:56 -0400

    image bg finally works. Let’s cleanup for launch!

--- lib/structure/archives.php
+++ lib/structure/archives.php
@@ -1,6 +1,10 @@
 <?php
 
-// Output the static blog page content before the posts
+/**
+ * Output the static blog page content before the posts.
+ *
+ * @return  void
+ */
 add_action( 'genesis_before_loop', 'mai_do_blog_description', 20 );
 function mai_do_blog_description() {
 
@@ -18,6 +22,8 @@ function mai_do_blog_description() {
  * Add term description before custom taxonomy loop.
  * This is the core WP term description, not the Genesis Intro Text.
  * Genesis Intro Text is in banner.
+ *
+ * @return  void
  */
 add_action( 'genesis_before_loop', 'mai_do_term_description', 20 );
 function mai_do_term_description() {
@@ -36,8 +42,13 @@ function mai_do_term_description() {
     }
 }
 
-add_action( 'genesis_before_loop', 'mai_maybe_remove_loop' );
-function mai_maybe_remove_loop() {
+/**
+ * Remove the loop if archive settings say so.
+ *
+ * @return  void
+ */
+add_action( 'genesis_before_loop', 'mai_remove_content_archive_loop' );
+function mai_remove_content_archive_loop() {
 
     // Bail if not a content archive
     if ( ! mai_is_content_archive() ) {
@@ -55,6 +66,11 @@ function mai_maybe_remove_loop() {
     remove_action( 'genesis_after_loop', 'genesis_posts_nav' );
 }
 
+/**
+ * Set the archive posts_per_page if we have a custom setting.
+ *
+ * @return void
+ */
 add_filter( 'pre_get_posts', 'mai_content_archive_posts_per_page' );
 function mai_content_archive_posts_per_page( $query ) {
 
@@ -80,9 +96,13 @@ function mai_content_archive_posts_per_page( $query ) {
         return;
     }
     $query->set( 'posts_per_page', $posts_per_page );
-    return $query;
 }
 
+/**
+ * Add the mai_before_flex_loop hook when appropriate.
+ *
+ * @return void
+ */
 add_action( 'genesis_before_while', 'mai_add_before_flex_loop_hook', 100 );
 function mai_add_before_flex_loop_hook() {
 
@@ -99,6 +119,11 @@ function mai_add_before_flex_loop_hook() {
     do_action( 'mai_before_flex_loop' );
 }
 
+/**
+ * Add the mai_after_flex_loop hook when appropriate.
+ *
+ * @return void
+ */
 add_action( 'genesis_after_endwhile', 'mai_add_after_flex_loop_hook' );
 function mai_add_after_flex_loop_hook() {
 
@@ -115,7 +140,12 @@ function mai_add_after_flex_loop_hook() {
     do_action( 'mai_after_flex_loop' );
 }
 
-// Flex loop opening html and filters
+/**
+ * Flex loop opening html and column filters.
+ * Add and remove the post/product class filters to create columns.
+ *
+ * @return  void
+ */
 add_action( 'mai_before_flex_loop', 'mai_do_flex_loop_before' );
 function mai_do_flex_loop_before() {
 
@@ -157,73 +187,10 @@ function mai_do_flex_loop_before() {
     });
 }
 
-add_action( 'genesis_before_entry', 'mai_do_entry_image_background' );
-function mai_do_entry_image_background() {
-
-    // Get image location
-    $image_location = mai_get_archive_setting( 'image_location', genesis_get_option( 'image_location' ) );
-
-    // If background image
-    if ( 'background' != $image_location ) {
-        return;
-    }
-
-    // Get the image ID
-    $image_id = get_post_thumbnail_id();
-
-    // Bail if no image
-    if ( ! $image_id ) {
-        return;
-    }
-
-    // Create an anonomous markup functions
-    $markup_open = function( $open ) {
-        $open = str_replace( '<article', '<a', $open );
-        return $open;
-    };
-    $markup_close = function( $close ) {
-        $close = str_replace( '</article>', '</a>', $close );
-        return $close;
-    };
-
-    // Get image size
-    $image_size = mai_get_archive_setting( 'image_size', genesis_get_option( 'image_size' ) );
-
-    // Anonomous attributes function
-    $entry_attributes = function( $attributes ) use ( $image_id, $image_size ) {
-
-        // Add classes and href link
-        $attributes['class'] .= ' overlay light-content center-xs middle-xs text-xs-center';
-        $attributes['href'] = get_permalink();
-
-        // Add image background attributes
-        $attributes = mai_add_image_background_attributes( $attributes, $image_id, $image_size );
-
-        return $attributes;
-    };
-
-    // Change entry markup from 'arcticle' to 'a'
-    add_filter( 'genesis_markup_entry_open', $markup_open );
-    add_filter( 'genesis_markup_entry_close', $markup_close );
-    add_filter( 'genesis_attr_entry', $entry_attributes );
-    add_filter( 'genesis_link_post_title', '__return_false' );
-    add_filter( 'genesis_markup_entry-content_open', '__return_false' );
-    add_filter( 'genesis_markup_entry-content_close', '__return_false' );
-
-    // Remove the filters so any other loops aren't affected
-    add_action( 'genesis_after_entry', function() use ( $markup_open, $markup_close, $entry_attributes ) {
-        remove_filter( 'genesis_markup_entry_open', $markup_open );
-        remove_filter( 'genesis_markup_entry_close', $markup_close );
-        remove_filter( 'genesis_attr_entry', $entry_attributes );
-        remove_filter( 'genesis_link_post_title', '__return_true' );
-        remove_filter( 'genesis_markup_entry-content_open', '__return_true' );
-        remove_filter( 'genesis_markup_entry-content_close', '__return_true' );
-    });
-
-}
-
 /**
- * Add the shortcode column count to the flex loop setting.
+ * Add the WooCommerce shortcode column count to the flex loop setting.
+ *
+ * @return  void
  */
 add_action( 'woocommerce_shortcode_before_recent_products_loop',       'mai_woo_shortcode_before_loop' );
 add_action( 'woocommerce_shortcode_before_sale_products_loop',         'mai_woo_shortcode_before_loop' );
@@ -285,15 +252,15 @@ function mai_woo_shortcode_before_loop( $atts ) {
 /**
  * Do the content archive options.
  * Hook in before the loop, get the variables first,
- * then pass them to the filter to avoid a redirect loop
+ * then pass them to the filters to avoid a redirect loop
  * since the helper function falls back to genesis_option() function.
  *
  * @since   1.0.0
  *
  * @return  void
  */
-add_action( 'genesis_before_loop', 'mai_do_archive_options' );
-function mai_do_archive_options() {
+add_action( 'genesis_before_while', 'mai_do_content_archive_archive_options' );
+function mai_do_content_archive_archive_options() {
 
     // Bail if not a content archive
     if ( ! mai_is_content_archive() ) {
@@ -310,14 +277,183 @@ function mai_do_archive_options() {
         remove_action( 'genesis_entry_content', 'genesis_do_post_content' );
     }
 
-    add_filter( 'genesis_options', function( $options ) use ( $content_archive_thumbnail, $image_size, $content_archive, $content_archive_limit ) {
-        $options['content_archive_thumbnail'] = $content_archive_thumbnail;
-        $options['image_size']                = $image_size;
-        $options['content_archive']           = $content_archive;
-        $options['content_archive_limit']     = absint( $content_archive_limit );
-        return $options;
+    // Image
+    add_filter( 'genesis_pre_get_option_content_archive_thumbnail', function( $options ) use ( $content_archive_thumbnail ) {
+        return $content_archive_thumbnail;
     });
 
+    // Image Size
+    add_filter( 'genesis_pre_get_option_image_size', function( $options ) use ( $image_size ) {
+        return $image_size;
+    });
+
+    // Content Archive
+    add_filter( 'genesis_pre_get_option_content_archive', function( $options ) use ( $content_archive ) {
+        return $content_archive;
+    });
+
+    // Archive Limit
+    add_filter( 'genesis_pre_get_option_content_archive_limit', function( $options ) use ( $content_archive_limit ) {
+        return $content_archive_limit;
+    });
+
+}
+
+/**
+ * Add the archive featured image in the correct location.
+ * No need to check if display image is checked, since that happens
+ * in the genesis_option filters already.
+ *
+ * @return  void
+ */
+add_action( 'genesis_before_while', 'mai_do_post_image' );
+function mai_do_post_image() {
+
+    // Bail if not a content archive
+    if ( ! mai_is_content_archive() ) {
+        return;
+    }
+
+    // Remove the post image
+    remove_action( 'genesis_entry_content', 'genesis_do_post_image', 8 );
+
+    $location = mai_get_archive_setting( 'image_location', genesis_get_option( 'more_link' ) );
+
+    // Bail if no location
+    if ( ! $location ) {
+        return;
+    }
+
+    /**
+     * Add the images in the correct location
+     */
+
+    // Before Entry
+    if ( 'before_entry' == $location ) {
+        add_action( 'genesis_entry_header', 'genesis_do_post_image', 2 );
+    }
+    // Before Title
+    elseif ( 'before_title' == $location ) {
+        add_action( 'genesis_entry_header', 'genesis_do_post_image', 8 );
+    }
+    // After Title
+    elseif ( 'after_title' == $location ) {
+        add_action( 'genesis_entry_header', 'genesis_do_post_image', 10 );
+    }
+    // Before Content
+    elseif ( 'before_content' == $location ) {
+        add_action( 'genesis_entry_content', 'genesis_do_post_image', 8 );
+    }
+    // Background Image
+    elseif ( 'background' == $location ) {
+        // Add the entry image as a background image
+        add_action( 'genesis_before_entry', 'mai_do_entry_image_background' );
+        // Remove the meta because we can't have nested links
+        remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
+        remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_open', 5 );
+        remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
+        remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_close', 15 );
+    }
+
+}
+
+/**
+ * Add the entry image as a background image.
+ * Change the markup to wrap the entire entry in an href link.
+ * Remove the title link.
+ *
+ * @return void
+ */
+function mai_do_entry_image_background() {
+
+    // Get the image ID
+    $image_id = get_post_thumbnail_id();
+
+    // Get image size
+    $image_size = mai_get_archive_setting( 'image_size', genesis_get_option( 'image_size' ) );
+
+    // Anonomous attributes function
+    $entry_attributes = function( $attributes ) use ( $image_id, $image_size ) {
+
+        // If we have an image
+        if ( $image_id ) {
+            // Add classes and href link
+            $attributes['class'] .= ' overlay light-content';
+            $attributes['href'] = get_permalink();
+            // Add image background attributes
+            $attributes = mai_add_image_background_attributes( $attributes, $image_id, $image_size );
+        }
+
+        // Center the content even if we don't have an image
+        $attributes['class'] .= ' center-xs middle-xs text-xs-center';
+
+        return $attributes;
+    };
+
+    // Add entry attributes
+    add_filter( 'genesis_attr_entry', $entry_attributes );
+
+    // Remove the filters so any other loops aren't affected
+    add_action( 'genesis_after_entry', function() use ( $entry_attributes ) {
+        remove_filter( 'genesis_attr_entry', $entry_attributes );
+    });
+
+    // If we have an image, change the entry markup
+    if ( $image_id ) {
+
+        // Create an anonomous markup functions
+        $markup_open = function( $open ) {
+            $open = str_replace( '<article', '<a', $open );
+            return $open;
+        };
+        $markup_close = function( $close ) {
+            $close = str_replace( '</article>', '</a>', $close );
+            return $close;
+        };
+
+        // Change entry markup from 'arcticle' to 'a'
+        add_filter( 'genesis_markup_entry_open', $markup_open );
+        add_filter( 'genesis_markup_entry_close', $markup_close );
+        add_filter( 'genesis_link_post_title', '__return_false' );
+
+        // Remove the filters so any other loops aren't affected
+        add_action( 'genesis_after_entry', function() use ( $markup_open, $markup_close, $entry_attributes ) {
+            remove_filter( 'genesis_markup_entry_open', $markup_open );
+            remove_filter( 'genesis_markup_entry_close', $markup_close );
+            remove_filter( 'genesis_link_post_title', '__return_true' );
+        });
+    }
+}
+
+/**
+ * Maybe remove the archive meta.
+ * Forces meta to be removed if image background setting.
+ *
+ * @return  void
+ */
+add_action( 'genesis_before_while', 'mai_archive_remove_meta' );
+function mai_archive_remove_meta() {
+
+    // Bail if not a content archive
+    if ( ! mai_is_content_archive() ) {
+        return;
+    }
+
+    // Get the meta to remove
+    $meta_to_remove = (array) mai_get_archive_setting( 'remove_meta', genesis_get_option( 'remove_meta' ) );
+
+    if ( in_array( 'post_info', $meta_to_remove ) ) {
+        // Remove the entry meta in the entry header
+        remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
+    }
+
+    if ( in_array( 'post_meta', $meta_to_remove ) ) {
+        // Remove the entry footer markup
+        remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_open', 5 );
+        remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_close', 15 );
+        // Remove the entry meta in the entry footer
+        remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
+    }
 }
 
 /**
@@ -358,74 +494,4 @@ function mai_do_more_link() {
         return;
     }
     echo mai_get_read_more_link( get_the_ID() );
-}
-
-/**
- * Add the archive featured image in the correct location.
- * No need to check if display image is checked, since that happens
- * in the genesis_option filters already.
- *
- * @return  void
- */
-add_action( 'genesis_before_while', 'mai_do_post_image' );
-function mai_do_post_image() {
-
-    // Bail if not a content archive
-    if ( ! mai_is_content_archive() ) {
-        return;
-    }
-
-    // Remove the post image
-    remove_action( 'genesis_entry_content', 'genesis_do_post_image', 8 );
-
-    $location = mai_get_archive_setting( 'image_location', genesis_get_option( 'more_link' ) );
-
-    // Bail if no location
-    if ( ! $location ) {
-        return;
-    }
-
-    // Add the images in the correct location
-    if ( 'before_entry' == $location ) {
-        add_action( 'genesis_entry_header', 'genesis_do_post_image', 2 );
-    } elseif ( 'before_title' == $location ) {
-        add_action( 'genesis_entry_header', 'genesis_do_post_image', 8 );
-    } elseif ( 'after_title' == $location ) {
-        add_action( 'genesis_entry_header', 'genesis_do_post_image', 10 );
-    } elseif ( 'before_content' == $location ) {
-        add_action( 'genesis_entry_content', 'genesis_do_post_image', 8 );
-    }
-
-}
-
-/**
- * Maybe remove the archive meta.
- * Forces meta to be removed if image background setting.
- *
- * @return  void
- */
-add_action( 'genesis_before_while', 'mai_archive_remove_meta' );
-function mai_archive_remove_meta() {
-
-    // Bail if not a content archive
-    if ( ! mai_is_content_archive() ) {
-        return;
-    }
-
-    // Get image location
-    $image_location = mai_get_archive_setting( 'image_location', genesis_get_option( 'image_location' ) );
-    $meta_to_remove = (array) mai_get_archive_setting( 'remove_meta', genesis_get_option( 'remove_meta' ) );
-
-    if ( 'background' == $image_location || in_array( 'post_info', $meta_to_remove ) ) {
-        // Remove the entry meta in the entry header
-        remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
-    }
-
-    if ( 'background' == $image_location || in_array( 'post_meta', $meta_to_remove ) ) {
-        // Remove the entry footer markup
-        remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_open', 5 );
-        remove_action( 'genesis_entry_footer', 'genesis_entry_footer_markup_close', 15 );
-        // Remove the entry meta in the entry footer
-        remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
-    }
 }
