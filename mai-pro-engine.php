1a2624d7de15604e3c76db97122db5b70100da78
Mike Hemberger <mike@thestizmedia.com>
2017-05-24 17:18:37 -0400

    v bump

--- mai-pro-engine.php
+++ mai-pro-engine.php
@@ -5,7 +5,7 @@
  * Plugin URI:      https://github.com/bizbudding/mai-pro-engine/
  * Description:     The Mai theme engine
  *
- * Version:         1.0.0-beta.16
+ * Version:         1.0.0-beta.17
  *
  * GitHub URI:      bizbudding/mai-pro-engine
  *
@@ -92,7 +92,7 @@ final class Mai_Pro_Engine {
 
         // Plugin version.
         if ( ! defined( 'MAI_PRO_ENGINE_VERSION' ) ) {
-            define( 'MAI_PRO_ENGINE_VERSION', '1.0.0-beta.16' );
+            define( 'MAI_PRO_ENGINE_VERSION', '1.0.0-beta.17' );
         }
 
         // Plugin Folder Path.
