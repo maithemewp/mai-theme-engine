9ca090056b6de01f8de4a1561126055b223cf53c
Mike Hemberger <mike@thestizmedia.com>
2017-07-21 11:35:00 -0400

    1.0.11-beta.1

--- mai-pro-engine.php
+++ mai-pro-engine.php
@@ -5,7 +5,7 @@
  * Plugin URI:      https://maitheme.com/
  * Description:     The Mai Pro Engine plugin
  *
- * Version:         1.0.10-beta.1
+ * Version:         1.0.11-beta.1
  *
  * GitHub URI:      bizbudding/mai-pro-engine
  *
@@ -92,7 +92,7 @@ final class Mai_Pro_Engine {
 
 		// Plugin version.
 		if ( ! defined( 'MAI_PRO_ENGINE_VERSION' ) ) {
-			define( 'MAI_PRO_ENGINE_VERSION', '1.0.10-beta.1' );
+			define( 'MAI_PRO_ENGINE_VERSION', '1.0.11-beta.1' );
 		}
 
 		// Plugin Folder Path.
