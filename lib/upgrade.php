e7c0958a3c16e3227a198c90518bcff083f72708
Mike Hemberger <mike@thestizmedia.com>
2017-09-07 10:56:27 -0400

    Much more full-proof upgrade script

--- lib/upgrade.php
+++ lib/upgrade.php
@@ -28,14 +28,13 @@ function mai_update_database_version() {
 }
 
 /**
- * Convert theme mods to theme settings (options).
+ * Convert theme mods and original settings to new theme settings (options).
  *
  * @since 1.1.0
  */
 function mai_upgrade_1100() {
 
-	// New settings.
-	$settings = array();
+	$settings = $mods_to_delete = array();
 
 	// Theme mods to convert to settings.
 	$mods = array(
@@ -56,6 +55,7 @@ function mai_upgrade_1100() {
 		$value = get_theme_mod( $key );
 		if ( $value ) {
 			$settings[$key] = $value;
+			$mods_to_delete[] = $key;
 		}
 	}
 
@@ -64,10 +64,10 @@ function mai_upgrade_1100() {
 		$settings['footer_widget_count'] = absint( $settings['footer_widget_count'] );
 	}
 
-	/**
-	 * These fields are going from boolean for all, to individual keys per post type.
-	 */
+	// This field is going from boolean for all, to individual keys per post type.
 	$banner_featured_image = get_theme_mod( 'banner_featured_image' );
+
+	// We need to deal with this value separately.
 	$enable_singular_image = get_theme_mod( 'enable_singular_image' );
 
 	// If enabled singular image is checked/true.
@@ -83,13 +83,18 @@ function mai_upgrade_1100() {
 	$disable_post_types = (array) genesis_get_option( 'banner_disable_post_types' );
 	$disable_taxonomies = (array) genesis_get_option( 'banner_disable_taxonomies' );
 
-	$settings['banner_disable_post_types'] = $settings['banner_disable_taxonomies'] = array();
+	$banner_disable_post_types = $banner_disable_taxonomies = array();
 
 	// Remove other non-default post types from this setting.
 	foreach( $disable_post_types as $post_type ) {
 		if ( in_array( $post_type, array( 'page', 'post' ) ) ) {
-			$settings['banner_disable_post_types'][] = $post_type;
+			$banner_disable_post_types[] = $post_type;
 		}
+	}
+
+	// If we have any, set them.
+	if ( $banner_disable_post_types ) {
+		$settings['banner_disable_post_types'] = (array) $banner_disable_post_types;
 	}
 
 	// Remove other non-post taxos from this setting.
@@ -109,10 +114,14 @@ function mai_upgrade_1100() {
 
 		foreach( $disable_taxonomies as $taxo ) {
 			if ( in_array( $taxo, $keeper_taxos ) ) {
-				$settings['banner_disable_taxonomies'][] = $taxo;
+				$banner_disable_taxonomies[] = $taxo;
 			}
 		}
 
+	}
+
+	if ( $banner_disable_taxonomies ) {
+		$settings['banner_disable_taxonomies'] = (array) $banner_disable_taxonomies;
 	}
 
 	// Get post types.
@@ -120,8 +129,10 @@ function mai_upgrade_1100() {
 
 	if ( $post_types ) {
 		foreach ( $post_types as $post_type => $object ) {
-			// Banner featured image.
-			$settings[ sprintf( 'banner_featured_image_%s', $post_type ) ] = $banner_featured_image;
+			if ( $banner_featured_image ) {
+				// Banner featured image.
+				$settings[ sprintf( 'banner_featured_image_%s', $post_type ) ] = $banner_featured_image;
+			}
 			// Banner disable post type.
 			if ( in_array( $post_type, $disable_post_types ) ) {
 				$settings[ sprintf( 'banner_disable_%s', $post_type ) ] = 1;
@@ -130,8 +141,9 @@ function mai_upgrade_1100() {
 			$taxonomies = get_object_taxonomies( $post_type, 'objects' );
 			if ( $taxonomies ) {
 				foreach ( $taxonomies as $name => $object ) {
-					if ( in_array( $name, $disable_taxonomies ) )
-					$settings[ sprintf( 'banner_disable_taxonomies_%s', $post_type ) ][] = $name;
+					if ( in_array( $name, $disable_taxonomies ) ) {
+						$settings[ sprintf( 'banner_disable_taxonomies_%s', $post_type ) ][] = $name;
+					}
 				}
 			}
 			// If we have a value from this theme_mod.
@@ -142,66 +154,86 @@ function mai_upgrade_1100() {
 		}
 	}
 
-	// Update.
-	if ( ! empty( $settings ) ) {
 
+	// Maybe update.
+	if ( ! empty( $settings ) ) {
 		// Update settings.
 		genesis_update_settings( $settings );
+	}
 
-		// Remove each theme mod from the array.
-		foreach ( $mods as $mod ) {
+	// Remove each theme mod from the array.
+	if ( $mods_to_delete ) {
+		foreach ( $mods_to_delete as $mod ) {
 			remove_theme_mod( $mod );
 		}
-		// enable_singular_image is not in the array because it was processed separately.
+	}
+
+	// This is not in array because it was dealt with separately.
+	if ( $enable_singular_image ) {
 		remove_theme_mod( 'enable_singular_image' );
 	}
 
 	// Static blog.
 	if ( $blog_page_id = get_option( 'page_for_posts' ) ) {
-		// Update the settings.
-		genesis_update_settings( _mai_upgrade_1100_get_static_archive_settings( $blog_page_id ), GENESIS_SETTINGS_FIELD );
+		$blog_settings = _mai_upgrade_1100_get_static_archive_settings( $blog_page_id );
+		if ( $blog_settings ) {
+			genesis_update_settings( $blog_settings, GENESIS_SETTINGS_FIELD );
+		}
 	}
 
 	// Woo upgrade, meta to cpt-archive-settings.
 	if ( class_exists( 'WooCommerce' ) && ( $shop_page_id = get_option( 'woocommerce_shop_page_id' ) ) ) {
-		$settings                = array();
-		$settings['banner_id']   = get_post_meta( $shop_page_id, 'banner_id', true );
-		$settings['hide_banner'] = get_post_meta( $shop_page_id, 'hide_banner', true );
-		$settings                = array_merge( $settings, _mai_upgrade_1100_get_static_archive_settings( $shop_page_id ) );
-		// Update the settings.
-		genesis_update_settings( $settings, GENESIS_CPT_ARCHIVE_SETTINGS_FIELD_PREFIX . 'product' );
+		$shop_settings = array();
+		if ( metadata_exists( 'post', $shop_page_id, 'banner_id' ) ) {
+			$shop_settings['banner_id']   = get_post_meta( $shop_page_id, 'banner_id', true );
+			delete_post_meta( $shop_page_id, 'banner_id' );
+		}
+		if ( metadata_exists( 'post', $shop_page_id, 'hide_banner' ) ) {
+			$shop_settings['hide_banner'] = get_post_meta( $shop_page_id, 'hide_banner', true );
+			delete_post_meta( $shop_page_id, 'hide_banner' );
+		}
+		$shop_settings = array_merge( $shop_settings, _mai_upgrade_1100_get_static_archive_settings( $shop_page_id ) );
+		if ( $shop_settings ) {
+			genesis_update_settings( $shop_settings, GENESIS_CPT_ARCHIVE_SETTINGS_FIELD_PREFIX . 'product' );
+		}
 	}
-
 
 }
 
 function _mai_upgrade_1100_get_static_archive_settings( $post_id ) {
-	$settings             = array();
-	$cpt_archive_settings = get_post_meta( $post_id, 'enabled_custom_archive_settings', true );
-	if ( $cpt_archive_settings ) {
-		$settings['columns']                   = get_post_meta( $post_id, 'columns', true );
-		$settings['content_archive']           = get_post_meta( $post_id, 'content_archive', true );
-		$settings['content_archive_thumbnail'] = get_post_meta( $post_id, 'content_archive_thumbnail', true );
-		$settings['content_archive_limit']     = get_post_meta( $post_id, 'columns', true );
-		$settings['image_location']            = get_post_meta( $post_id, 'image_location', true );
-		$settings['image_size']                = get_post_meta( $post_id, 'image_size', true );
-		$settings['image_alignment']           = get_post_meta( $post_id, 'image_alignment', true );
-		$settings['more_link']                 = get_post_meta( $post_id, 'more_link', true );
-		$settings['remove_meta']               = get_post_meta( $post_id, 'remove_meta', true );
-		$settings['posts_per_page']            = get_post_meta( $post_id, 'posts_per_page', true );
-		$settings['posts_nav']                 = get_post_meta( $post_id, 'posts_nav', true );
+
+	$settings = array();
+
+	if ( metadata_exists( 'post', $post_id, 'enabled_custom_archive_settings' ) ) {
+
+		$meta = array(
+			'columns',
+			'content_archive',
+			'content_archive_thumbnail',
+			'content_archive_limit',
+			'image_location',
+			'image_size',
+			'image_alignment',
+			'more_link',
+			'remove_meta',
+			'posts_per_page',
+			'posts_nav',
+		);
+
+		$cpt_archive_settings = get_post_meta( $post_id, 'enabled_custom_archive_settings', true );
+
+		if ( $cpt_archive_settings ) {
+			foreach ( $meta as $key ) {
+				if ( metadata_exists( 'post', $post_id, $key ) ) {
+					$settings[$key] = get_post_meta( $post_id, $key, true );
+					delete_post_meta( $post_id, $key );
+				}
+			}
+		}
+
+		delete_post_meta( $post_id, 'enabled_custom_archive_settings' );
+
 	}
-	delete_post_meta( $post_id, 'enabled_custom_archive_settings' );
-	delete_post_meta( $post_id, 'columns' );
-	delete_post_meta( $post_id, 'content_archive' );
-	delete_post_meta( $post_id, 'content_archive_thumbnail' );
-	delete_post_meta( $post_id, 'content_archive_limit' );
-	delete_post_meta( $post_id, 'image_location' );
-	delete_post_meta( $post_id, 'image_size' );
-	delete_post_meta( $post_id, 'image_alignment' );
-	delete_post_meta( $post_id, 'more_link' );
-	delete_post_meta( $post_id, 'remove_meta' );
-	delete_post_meta( $post_id, 'posts_per_page' );
-	delete_post_meta( $post_id, 'posts_nav' );
+
 	return $settings;
 }
