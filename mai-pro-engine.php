07759dbb99066ddcb71c42a8c2aafc36073bcbf8
Mike Hemberger <mike@thestizmedia.com>
2017-09-11 11:59:07 -0400

    v bump

--- mai-pro-engine.php
+++ mai-pro-engine.php
@@ -5,7 +5,7 @@
  * Plugin URI:      https://maipro.io/
  * Description:     The Mai Pro Engine plugin
  *
- * Version:         1.1.0.1
+ * Version:         1.1.1
  *
  * GitHub URI:      maiprowp/mai-pro-engine
  *
@@ -91,7 +91,7 @@ final class Mai_Pro_Engine {
 	private function setup_constants() {
 
 		// Plugin version.
-		define( 'MAI_PRO_ENGINE_VERSION', '1.1.0.1' );
+		define( 'MAI_PRO_ENGINE_VERSION', '1.1.1' );
 
 		// DB version.
 		define( 'MAI_PRO_ENGINE_DB_VERSION', '1100' );
