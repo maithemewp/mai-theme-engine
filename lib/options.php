57d66f1aeac3a1164427b6340f785e4ff8329606
Mike Hemberger <mike@thestizmedia.com>
2017-09-15 12:02:12 -0400

    Some default settings were getting changed…
     when updating/saving via Genesis > Theme Settings.

--- lib/options.php
+++ lib/options.php
@@ -9,23 +9,34 @@
  *
  * @return  array
  */
-add_filter( 'pre_update_option_genesis-settings', 'mai_enforce_custom_genesis_settings', 99, 2 );
+add_filter( 'pre_update_option_genesis-settings', 'mai_enforce_custom_genesis_settings', 10, 2 );
 function mai_enforce_custom_genesis_settings( $new_value, $old_value ) {
+
+	// Bail if this isn't happening from a form submission page.
+	if ( ! isset( $_POST ) || empty( $_POST ) ) {
+		return $new_value;
+	}
+
+	// Bail if this isn't happening on a page that's submitting a 'genesis-settings' form.
+	if ( ! isset( $_POST['genesis-settings'] ) || empty( $_POST['genesis-settings'] ) ) {
+		return $new_value;
+	}
+
+	// Get the submitted and existing settings values.
+	$values   = $_POST[ 'genesis-settings' ];
 	$settings = get_option( 'genesis-settings' );
-	foreach ( (array) $settings as $setting => $value ) {
+
+	// Loop through em.
+	foreach ( (array) $settings as $key => $value ) {
 		/**
-		 * Header/Footer scripts get wp_slash sanitization automatically, we need to unslash before that gets re-applied.
-		 *
-		 * @see  genesis/lib/admin/theme-settings save() method.
+		 * If a custom setting is not part of the $_POST submission,
+		 * we need to add to the $new_value array it so it's not lost.
 		 */
-		if ( in_array( $setting, array( 'header_scripts', 'footer_scripts' ) ) ) {
-			$new_value[ $setting ] = wp_unslash( $value );
-		}
-		// If a custom setting is not here, we need to add it so it's not lost.
-		if ( ! isset( $new_value[ $setting ] ) ) {
-			$new_value[ $setting ] = $value;
+		if ( ! isset( $values[ $key ] ) ) {
+			$new_value[ $key ] = genesis_get_option( $key );
 		}
 	}
+
 	return $new_value;
 }
 
