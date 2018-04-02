71652eb6bf94ecf8b3c06d7211018524a1b1e30c
Mike Hemberger <mike@thestizmedia.com>
2018-04-02 00:19:30 -0400

    Start re-registering shortcodes.
    Still need to do col_*

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -12,52 +12,49 @@ add_filter( 'genesis_author_intro_text_output', 'do_shortcode' );
 // Term Archive Intro Text.
 add_filter( 'genesis_term_intro_text_output', 'do_shortcode' );
 
-// TODO: Register all shortcodes here and instantiate classes!
-
-/**
- * Filter the content to remove empty <p></p> tags and extray <br /> added by shortcodes.
- *
- * @link https://gist.github.com/bitfade/4555047
- *
- * @return  mixed  Fixed shortcode content
- */
+// Filter the content.
 add_filter( 'the_content', 'mai_content_filter_shortcodes' );
-function mai_content_filter_shortcodes( $content ) {
 
-	$shortcodes = array(
-		'callout',
-		'section',
-		'columns',
-		'col',
-		'col_auto',
-		'col_one_twelfth',
-		'col_one_sixth',
-		'col_one_fourth',
-		'col_one_third',
-		'col_five_twelfths',
-		'col_one_half',
-		'col_seven_twelfths',
-		'col_two_thirds',
-		'col_three_fourths',
-		'col_five_sixths',
-		'col_eleven_twelfths',
-		'col_one_whole',
-		'grid',
-	);
+add_shortcode( 'callout',              'mai_get_callout_shortcode' );
+add_shortcode( 'grid',                 'mai_get_grid_shortcode' );
+add_shortcode( 'section',              'mai_get_section_shortcode' );
+add_shortcode( 'columns',              'mai_get_columns_shortcode' );
+add_shortcode( 'col',                  'mai_get_col_shortcode' );
+add_shortcode( 'col_auto',             'mai_get_col_auto_shortcode' );
+add_shortcode( 'col_one_twelfth',      'mai_get_col_one_twelfth_shortcode' );
+add_shortcode( 'col_one_sixth',        'mai_get_col_one_sixth_shortcode' );
+add_shortcode( 'col_one_fourth',       'mai_get_col_one_fourth_shortcode' );
+add_shortcode( 'col_one_third',        'mai_get_col_one_third_shortcode' );
+add_shortcode( 'col_five_twelfths',    'mai_get_col_five_twelfths_shortcode' );
+add_shortcode( 'col_one_half',         'mai_get_col_one_half_shortcode' );
+add_shortcode( 'col_seven_twelfths',   'mai_get_col_seven_twelfths_shortcode' );
+add_shortcode( 'col_two_thirds',       'mai_get_col_two_thirds_shortcode' );
+add_shortcode( 'col_three_fourths',    'mai_get_col_three_fourths_shortcode' );
+add_shortcode( 'col_five_sixths',      'mai_get_col_five_sixths_shortcode' );
+add_shortcode( 'col_eleven_twelfths',  'mai_get_col_eleven_twelfths_shortcode' );
+add_shortcode( 'col_one_whole',        'mai_get_col_one_whole_shortcode' );
 
-	// Array of custom shortcodes requiring the fix.
-	$shortcodes = join( '|', $shortcodes );
+function mai_get_callout_shortcode( $atts, $content = null ) {
+	$callout = new Mai_Callout( $atts, $content );
+	return $callout->render();
+}
 
-	// Cleanup.
-	$content = strtr( $content, array ( '<p></p>' => '', '<p>[' => '[', ']</p>' => ']', ']<br />' => ']' ) );
+function mai_get_grid_shortcode( $atts, $content = null ) {
+	$grid = new Mai_Grid( $atts, $content );
+	return $grid->render();
+}
 
-	// Opening tag.
-	$content = preg_replace( "/(<p>)?\[($shortcodes)(\s[^\]]+)?\](<\/p>|<br \/>)?/", "[$2$3]", $content );
+function mai_get_section_shortcode( $atts, $content = null ) {
+	$section = new Mai_Section( $atts, $content );
+	return $section->render();
+}
 
-	// Closing tag.
-	$content = preg_replace( "/(<p>)?\[\/($shortcodes)](<\/p>|<br \/>)?/", "[/$2]", $content );
+function mai_get_columns_shortcode( $atts, $content = null ) {
+	$columns = new Mai_Columns( $atts, $content );
+	return $columns->render();
+}
 
-	// Return fixed shortcodes.
-	return $content;
-
+function mai_get_col_shortcode( $atts, $content = null ) {
+	$col = new Mai_Col( $atts, $content );
+	return $col->render();
 }
