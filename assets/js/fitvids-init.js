5fe6ff1750cc23d2090c8edeb811b2927ac26f99
Mike Hemberger <mike@thestizmedia.com>
2018-05-30 21:31:48 -0400

    1.3.0-beta.9

--- assets/js/fitvids-init.js
+++ assets/js/fitvids-init.js
@@ -1,8 +1,6 @@
 /**
- * Initiate Slick on [grid slider="true"] posts when document ready.
- *
- * @version 1.0.2
+ * Initiate FitVids.
  */
-jQuery(document).ready(function($) {
+jQuery( document ).ready( function($) {
 	$( '.site-container' ).fitVids();
 });
