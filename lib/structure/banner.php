35a6ad7f1f4222fc15cc1aa96c2652b002540cf0
Mike Hemberger <mike@thestizmedia.com>
2018-06-18 14:30:12 -0400

    fix textdomain in string

--- lib/structure/banner.php
+++ lib/structure/banner.php
@@ -213,7 +213,7 @@ function mai_do_banner_content() {
 
 	// 404.
 	elseif ( is_404() ) {
-		$title = __( '404', 'genesis' );
+		$title = __( '404', 'mai-theme-engine' );
 	}
 
 	// Banner content filters.
