6daeeb5629d6fb237eb6100902feca0fb48ecc67
Mike Hemberger <mike@thestizmedia.com>
2018-07-13 15:46:09 -0400

    site header search bar CSS to larger screens
    https://github.com/maithemewp/mai-theme-engine/issues/229

--- mai-theme-engine.php
+++ mai-theme-engine.php
@@ -5,7 +5,7 @@
  * Plugin URI:      https://maitheme.com/
  * Description:     The Mai Theme Engine plugin
  *
- * Version:         1.3.6
+ * Version:         1.3.7
  *
  * GitHub URI:      maithemewp/mai-theme-engine
  *
@@ -89,7 +89,7 @@ final class Mai_Theme_Engine {
 	private function setup_constants() {
 
 		// Plugin version.
-		define( 'MAI_THEME_ENGINE_VERSION', '1.3.6' );
+		define( 'MAI_THEME_ENGINE_VERSION', '1.3.7' );
 
 		// DB version.
 		define( 'MAI_THEME_ENGINE_DB_VERSION', '1161' );
