38f7e4b117eafa5e4c903c5f2d7b2990030b1cc8
Mike Hemberger <mike@thestizmedia.com>
2018-12-25 21:58:44 -0500

    Much more efficient scroll listener
    Everything based on headroom now

--- assets/js/mai-theme.js
+++ assets/js/mai-theme.js
@@ -22,29 +22,36 @@
  */
 ( function( document, $, undefined ) {
 
-	var $body       = $( 'body' );
+	var $body       = $( 'body' ),
 		$window     = $(window),
-		$container  = $( '.site-container' ),
 		$header     = $( '.site-header' ),
 		$customLogo = $header.find( '.custom-logo-link' ),
 		$titleText  = $header.find( '.site-title a' ).not( '.custom-logo-link' );
 
-	var hasShrink       = $body.hasClass( 'has-shrink-header' ),
-		hasStickyShrink = $body.hasClass( 'has-sticky-shrink-header' ),
-		hasReveal       = $body.hasClass( 'has-reveal-header' ),
-		hasRevealShrink = $body.hasClass( 'has-reveal-shrink-header' ),
-		fontSize        = parseInt( $titleText.css( 'font-size' ) ),
-		logoWidth       = $customLogo.outerWidth();
+	var hasShrink = $body.hasClass( 'has-shrink-header' ),
+		hasReveal = $body.hasClass( 'has-reveal-header' ),
+		fontSize  = parseInt( $titleText.css( 'font-size' ) ),
+		logoWidth = $customLogo.outerWidth();
 
-	// Construct an instance of Headroom, passing the element.
-	var bodyScroll = new Headroom( document.querySelector( 'body' ), {
+	// If shrinking header and on mobile/tablet size window.
+	if ( hasShrink && $window.width() <= 768 ) {
+		shrinkHeader();
+	}
+	$window.on( 'resize orientationchange', function() {
+		if ( hasShrink && $window.width() <= 768 ) {
+			shrinkHeader();
+		}
+	});
+
+	var $vanillaBody       = document.querySelector( 'body' );
+	var vanillaBodyOptions = {
 		offset: 1,
 		classes : {
 			initial : 'headroom',
-			pinned : 'pinned',
-			unpinned : 'unpinned',
-			top : 'scroll',
-			notTop : 'not-scroll',
+			pinned: 'scroll-up',
+			unpinned: 'scroll-down',
+			top : 'not-scroll',
+			notTop : 'scroll',
 			bottom : 'bottom',
 			notBottom : 'not-bottom'
 		},
@@ -54,15 +61,13 @@
 		onUnpin : function() {},
 		// when above offset.
 		onTop : function() {
-			// $body.removeClass( 'scroll' );
-			if ( hasShrink ) {
+			if ( shouldShrink() ) {
 				unshrinkHeader();
 			}
 		},
 		// when below offset.
 		onNotTop : function() {
-			// $body.addClass( 'scroll' );
-			if ( hasShrink ) {
+			if ( shouldShrink() ) {
 				shrinkHeader();
 			}
 		},
@@ -70,256 +75,63 @@
 		onBottom : function() {},
 		// when not at bottom of scroll area.
 		onNotBottom : function() {}
-	});
+	}
+
+	// Construct an instance of Headroom, passing the element.
+	var bodyScroll = new Headroom( $vanillaBody, vanillaBodyOptions );
 
 	// Initialise bodyScroll.
 	bodyScroll.init();
 
 	if ( hasReveal ) {
 
-		var headerScroll = new Headroom( document.querySelector( 'header' ), {
-			offset: 256,
-			tolerance: {
-				down: 5,
-				up: 15
-			},
+		var $vanillaHeader       = document.querySelector( 'header' );
+		var vanillaHeaderOptions = {
+			tolerance: 5,
 			classes : {
 				initial : 'headroom',
-				pinned : 'reveal-header',
-				unpinned : 'conceal-header',
-				top : 'top',
-				notTop : 'nop-top',
+				pinned: 'reveal-header',
+				unpinned: 'conceal-header',
+				top : 'not-scroll',
+				notTop : 'scroll',
 				bottom : 'bottom',
 				notBottom : 'not-bottom'
 			},
-		});
+			// when scrolling up.
+			onPin : function() {},
+			// when scrolling down.
+			onUnpin : function() {},
+			// when above offset.
+			onTop : function() {
+				// if ( shouldShrink() ) {
+				// 	unshrinkHeader();
+				// }
+			},
+			// when below offset.
+			onNotTop : function() {},
+			// when at bottom of scoll area.
+			onBottom : function() {},
+			// when not at bottom of scroll area.
+			onNotBottom : function() {}
+		}
+		var headerScroll = new Headroom( $vanillaHeader, vanillaHeaderOptions );
 
 		// Initialise headerScroll.
 		headerScroll.init();
-	}
 
-	// If doing a sticky shrink header.
-	if ( hasStickyShrink ) {
-
-		// var	shrinkFired      = false,
-		// 	unshrinkFired    = false,
-		// 	titleShrinkFired = false;
-
-		// $window.on( 'orientationchange resize scroll', function() {
-
-		// 	var windowWidth = $window.width();
-
-		// 	// Larger browser windows.
-		// 	if ( windowWidth > 768 ) {
-
-		// 		// Shrink/Unshrink triggers.
-		// 		if ( $window.scrollTop() > 1 ) {
-		// 			if ( false !== shrinkFired ) {
-		// 				return;
-		// 			}
-		// 			shrinkHeader();
-		// 			shrinkFired   = true;
-		// 			unshrinkFired = false;
-		// 		} else {
-		// 			if ( false !== unshrinkFired ) {
-		// 				return;
-		// 			}
-		// 			unshrinkHeader();
-		// 			unshrinkFired = true;
-		// 			shrinkFired   = false;
-		// 		}
-
-		// 		// Unset this incase browser resized small to large.
-		// 		titleShrinkFired = false;
-
-		// 	}
-		// 	// Smaller browser windows.
-		// 	else {
-
-		// 		if ( ! titleShrinkFired ) {
-
-		// 			// Force shrink text on wall windows.
-		// 			shrinkTitle();
-		// 			shrinkLogo();
-		// 		}
-
-		// 		titleShrinkFired = true;
-		// 	}
-
-		// });
-
-		doShrinkHeader();
-
-	}
-	// If doing reveal header.
-	else if ( hasReveal ) {
-
-		// Get the header.
-		// var $siteHeader = document.querySelector( 'header' );
-
-		/**
- 		 * headroom.js v0.9.4 - Give your page some headroom. Hide your header until you need it.
- 		 * Copyright (c) 2017 Nick Williams - http://wicky.nillia.ms/headroom.js
-		 * License: MIT
-		 */
-		// !function(a,b){"use strict";"function"==typeof define&&define.amd?define([],b):"object"==typeof exports?module.exports=b():a.Headroom=b()}(this,function(){"use strict";function a(a){this.callback=a,this.ticking=!1}function b(a){return a&&"undefined"!=typeof window&&(a===window||a.nodeType)}function c(a){if(arguments.length<=0)throw new Error("Missing arguments in extend function");var d,e,f=a||{};for(e=1;e<arguments.length;e++){var g=arguments[e]||{};for(d in g)"object"!=typeof f[d]||b(f[d])?f[d]=f[d]||g[d]:f[d]=c(f[d],g[d])}return f}function d(a){return a===Object(a)?a:{down:a,up:a}}function e(a,b){b=c(b,e.options),this.lastKnownScrollY=0,this.elem=a,this.tolerance=d(b.tolerance),this.classes=b.classes,this.offset=b.offset,this.scroller=b.scroller,this.initialised=!1,this.onPin=b.onPin,this.onUnpin=b.onUnpin,this.onTop=b.onTop,this.onNotTop=b.onNotTop,this.onBottom=b.onBottom,this.onNotBottom=b.onNotBottom}var f={bind:!!function(){}.bind,classList:"classList"in document.documentElement,rAF:!!(window.requestAnimationFrame||window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame)};return window.requestAnimationFrame=window.requestAnimationFrame||window.webkitRequestAnimationFrame||window.mozRequestAnimationFrame,a.prototype={constructor:a,update:function(){this.callback&&this.callback(),this.ticking=!1},requestTick:function(){this.ticking||(requestAnimationFrame(this.rafCallback||(this.rafCallback=this.update.bind(this))),this.ticking=!0)},handleEvent:function(){this.requestTick()}},e.prototype={constructor:e,init:function(){if(e.cutsTheMustard)return this.debouncer=new a(this.update.bind(this)),this.elem.classList.add(this.classes.initial),setTimeout(this.attachEvent.bind(this),100),this},destroy:function(){var a=this.classes;this.initialised=!1;for(var b in a)a.hasOwnProperty(b)&&this.elem.classList.remove(a[b]);this.scroller.removeEventListener("scroll",this.debouncer,!1)},attachEvent:function(){this.initialised||(this.lastKnownScrollY=this.getScrollY(),this.initialised=!0,this.scroller.addEventListener("scroll",this.debouncer,!1),this.debouncer.handleEvent())},unpin:function(){var a=this.elem.classList,b=this.classes;!a.contains(b.pinned)&&a.contains(b.unpinned)||(a.add(b.unpinned),a.remove(b.pinned),this.onUnpin&&this.onUnpin.call(this))},pin:function(){var a=this.elem.classList,b=this.classes;a.contains(b.unpinned)&&(a.remove(b.unpinned),a.add(b.pinned),this.onPin&&this.onPin.call(this))},top:function(){var a=this.elem.classList,b=this.classes;a.contains(b.top)||(a.add(b.top),a.remove(b.notTop),this.onTop&&this.onTop.call(this))},notTop:function(){var a=this.elem.classList,b=this.classes;a.contains(b.notTop)||(a.add(b.notTop),a.remove(b.top),this.onNotTop&&this.onNotTop.call(this))},bottom:function(){var a=this.elem.classList,b=this.classes;a.contains(b.bottom)||(a.add(b.bottom),a.remove(b.notBottom),this.onBottom&&this.onBottom.call(this))},notBottom:function(){var a=this.elem.classList,b=this.classes;a.contains(b.notBottom)||(a.add(b.notBottom),a.remove(b.bottom),this.onNotBottom&&this.onNotBottom.call(this))},getScrollY:function(){return void 0!==this.scroller.pageYOffset?this.scroller.pageYOffset:void 0!==this.scroller.scrollTop?this.scroller.scrollTop:(document.documentElement||document.body.parentNode||document.body).scrollTop},getViewportHeight:function(){return window.innerHeight||document.documentElement.clientHeight||document.body.clientHeight},getElementPhysicalHeight:function(a){return Math.max(a.offsetHeight,a.clientHeight)},getScrollerPhysicalHeight:function(){return this.scroller===window||this.scroller===document.body?this.getViewportHeight():this.getElementPhysicalHeight(this.scroller)},getDocumentHeight:function(){var a=document.body,b=document.documentElement;return Math.max(a.scrollHeight,b.scrollHeight,a.offsetHeight,b.offsetHeight,a.clientHeight,b.clientHeight)},getElementHeight:function(a){return Math.max(a.scrollHeight,a.offsetHeight,a.clientHeight)},getScrollerHeight:function(){return this.scroller===window||this.scroller===document.body?this.getDocumentHeight():this.getElementHeight(this.scroller)},isOutOfBounds:function(a){var b=a<0,c=a+this.getScrollerPhysicalHeight()>this.getScrollerHeight();return b||c},toleranceExceeded:function(a,b){return Math.abs(a-this.lastKnownScrollY)>=this.tolerance[b]},shouldUnpin:function(a,b){var c=a>this.lastKnownScrollY,d=a>=this.offset;return c&&d&&b},shouldPin:function(a,b){var c=a<this.lastKnownScrollY,d=a<=this.offset;return c&&b||d},update:function(){var a=this.getScrollY(),b=a>this.lastKnownScrollY?"down":"up",c=this.toleranceExceeded(a,b);this.isOutOfBounds(a)||(a<=this.offset?this.top():this.notTop(),a+this.getViewportHeight()>=this.getScrollerHeight()?this.bottom():this.notBottom(),this.shouldUnpin(a,c)?this.unpin():this.shouldPin(a,c)&&this.pin(),this.lastKnownScrollY=a)}},e.options={tolerance:{up:0,down:0},offset:0,scroller:window,classes:{pinned:"headroom--pinned",unpinned:"headroom--unpinned",top:"headroom--top",notTop:"headroom--not-top",bottom:"headroom--bottom",notBottom:"headroom--not-bottom",initial:"headroom"}},e.cutsTheMustard="undefined"!=typeof f&&f.rAF&&f.bind&&f.classList,e});
-
-
-		// new Waypoint({
-		// 	element: $container,
-		// 	offset: 200,
-		// 	handler: function( direction ) {
-		// 		if ( 'down' === direction ) {
-		// 			$window.trigger( 'maiHeaderConceal' );
-		// 		}
-		// 	}
-		// });
-
-		// var previousTop  = 0,
-		// 	currentTop   = 0,
-		// 	scrollDelta  = 16,
-		// 	scrollOffset = 256;
-
-		// $window.on( 'maiThrottledScroll', function() {
-		// 	var currentTop = $window.scrollTop();
-		// 	maybeRevealHeader( currentTop );
-		// 	previousTop = currentTop;
-		// 	scrolling   = false;
-		// });
-		// function maybeRevealHeader( currentTop ) {
-		// 	if ( previousTop - currentTop > scrollDelta ) {
-		// 		$window.trigger( 'maiHeaderReveal' );
-		// 	} else if ( currentTop - previousTop > scrollDelta && currentTop > scrollOffset ) {
-		// 		$window.trigger( 'maiHeaderConceal' );
-		// 	}
-		// }
-
-		var shouldNotScroll = false;
-
-		/**
- 		 * Temporarily disable the scroll function
-		 * when clicking on anything in the header.
-		 *
-		 * If the mobile menu is activated, destroy headroom.
-		 */
-		$header.on( 'click', function() {
-
-			shouldNotScroll = true;
-			setTimeout( function() {
-				shouldNotScroll = false;
-			}, 300 );
-
-			// If mobile menu is activated.
-			if ( $body.hasClass( 'mai-menu-activated' ) ) {
-
-				// Deactivate headroom.
-				// headroom.destroy();
-			}
-			// If mobile menu not activated and headroom is not initialized.
-			else if ( ! $header.hasClass( 'headroom' ) ) {
-
-				// Initialize.
-				setTimeout( function() {
-					// headroom.init();
-				}, 300 );
-			}
-		});
-
-		// Bail if not shrinking.
-		if ( ! hasRevealShrink ) {
-			return;
-		}
-
-		// $window.on( 'orientationchange resize scroll', function() {
-
-		// 	// Bail if not monitoring scroll.
-		// 	if ( shouldNotScroll ) {
-		// 		return;
-		// 	}
-
-		// 	// Bail if the mobile menu is open. Typically when scrolling with mobile menu open.
-		// 	// body lock scroll makes this not matter?!?
-		// 	// if ( $body.hasClass( 'mai-menu-activated' ) ) {
-		// 	// 	return;
-		// 	// }
-
-		// 	// Bail if smaller window, since everything will always be shrunk.
-		// 	if ( $window.width() <= 768 ) {
-		// 		return;
-		// 	}
-
-		// 	// Current scroll position and window width.
-		// 	var scrollTop = $window.scrollTop();
-
-		// 	// Scrolled to the top.
-		// 	if ( scrollTop <= 1 ) {
-		// 		unshrinkHeader();
-		// 	}
-		// 	// Scrolling either direction and header is not shrunk.
-		// 	else if ( ! $header.hasClass( 'shrink' ) ) {
-		// 		shrinkHeader();
-		// 	}
-
-		// });
-
-		doShrinkHeader();
-
-	}
-
-	function doShrinkHeader() {
-
-		if ( $window.width() <= 768 ) {
-			shrinkHeader();
-		}
-
+		// Set offset based on header size.
+		headerScroll.offset = $header.height() + 240;
 		$window.on( 'resize orientationchange', function() {
-			shrinkHeader();
+			headerScroll.offet = $header.height() + 240;
 		});
-
-		// new Waypoint({
-		// 	element: $container,
-		// 	offset: 0,
-		// 	handler: function( direction ) {
-		// 		console.log( direction );
-		// 		if ( $window.width() > 768 ) {
-		// 			if ( 'up' === direction ) {
-		// 				unshrinkHeader();
-		// 			}
-		// 		} else {
-		// 			shrinkHeader();
-		// 		}
-		// 	}
-		// });
-
-		// new Waypoint({
-		// 	element: $container,
-		// 	offset: 1,
-		// 	handler: function( direction ) {
-		// 		if ( $window.width() > 768 ) {
-		// 			shrinkHeader();
-		// 		}
-		// 	}
-		// });
-		// $window.on( 'maiOnTop', function() {
-		// 	if ( $window.width() <= 768 ) {
-		// 		shrinkHeader();
-		// 	});
-		// });
-		// $window.on( 'maiNotTop', function() {
-		// 	if ( $window.width() > 768 ) {
-		// 		shrinkHeader();
-		// 	}
-		// });
-		// $window.on( 'maiToTop', function() {
-		// 	if ( $window.width() > 768 ) {
-		// 		unshrinkHeader();
-		// 	}
-		// });
 	}
 
 	/* ****** *
 	 * Shrink *
 	 * ****** */
+	function shouldShrink() {
+		return ( hasShrink && $window.width() > 768 );
+	}
 
 	function shrinkHeader() {
 		$header.addClass( 'shrink' );
@@ -460,6 +272,9 @@
 
 		var $this       = $(this),
 			hasSideMenu = $body.hasClass( 'has-side-menu' );
+
+		// Remove reveal/conceal classes cause they cause animation issues.
+		$siteHeader.removeClass( 'conceal-header reveal-header' );
 
 		// Toggle the mobile menu activated.
 		$this._toggleActive();
