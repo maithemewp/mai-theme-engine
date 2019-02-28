0be3f516358a634f6c95e95eb93ac1b16d76affa
Mike Hemberger <mike@thestizmedia.com>
2019-02-28 16:57:22 -0500

    Track body, not header in basicScroll.

--- assets/js/mai-scroll.js
+++ assets/js/mai-scroll.js
@@ -118,7 +118,7 @@ document.addEventListener( 'DOMContentLoaded', function() {
 		headerTo     = headerFrom + 200;
 
 	var headerScroll = basicScroll.create({
-		elem: header,
+		elem: body,
 		from: headerFrom,
 		to: headerTo,
 		// We need really large values to force whole numbers and partially help jitters/jank.
