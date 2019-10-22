85ce74a3cff4e49e14ca8ac31656150fa0bfa496
Mike Hemberger <mike@thestizmedia.com>
2019-10-22 16:56:22 -0400

    1.11.0-beta.11

--- mai-theme-engine.php
+++ mai-theme-engine.php
@@ -5,7 +5,7 @@
  * Plugin URI:      https://maitheme.com/
  * Description:     The Mai Theme Engine plugin
  *
- * Version:         1.11.0-beta.9
+ * Version:         1.11.0-beta.11
  *
  * GitHub URI:      maithemewp/mai-theme-engine
  *
@@ -89,7 +89,7 @@ final class Mai_Theme_Engine {
 	private function setup_constants() {
 
 		// Plugin version.
-		define( 'MAI_THEME_ENGINE_VERSION', '1.11.0-beta.9' );
+		define( 'MAI_THEME_ENGINE_VERSION', '1.11.0-beta.11' );
 
 		// DB version.
 		define( 'MAI_THEME_ENGINE_DB_VERSION', '1400' );
